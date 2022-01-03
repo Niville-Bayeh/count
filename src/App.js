c479dc41360558ccc326875d72a2bd56a9464262
niville <bayehniville@gmail.com>
2022-01-03 08:44:05 +0100

    modified app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const[count] = useState(0);
   return (
     <div className="container">
       <div className='card text-center my-5'>
         <div className='card-body'>
           <h1>Counter App</h1>
+          <div className='my-5'>
+            <h1 className='my-5'>{count}</h1>
+            <button className='btn btn-success mx-3'>Increment</button>
+            <button className='btn btn-danger mx-3'>Decrement</button>
+            <button className='btn btn-secondary mx-3'>Reset</button>
+          </div>
         </div>
       </div>
     </div>
