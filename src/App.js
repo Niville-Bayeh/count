628698debb2913c8f991b56dd7187e71dd57bad5
niville <bayehniville@gmail.com>
2022-01-03 15:39:46 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,22 @@ function App() {
           <h1>Counter App</h1>
           <div className='my-5'>
             <h1 className='my-5'>{count}</h1>
-            <button className='btn btn-success mx-3' onClick={() => setCount (count + 1)}>Increment</button>
-            <button className='btn btn-danger mx-3'onClick={() => setCount (count - 1)}>Decrement</button>
-            <button className='btn btn-secondary mx-3'onClick={() => setCount(0)}>Reset</button>
+
+            <button className='btn btn-success mx-3'
+            onClick={() => setCount (count + 1)}
+            >
+              Increment</button>
+
+            <button className='btn btn-danger mx-3'
+            onClick={() => setCount (count - 1)}
+            >
+              Decrement</button>
+
+            <button className='btn btn-secondary mx-3'
+            onClick={() => setCount(0)}
+            >
+              Reset</button>
+              
           </div>
         </div>
       </div>
