49e7d9f4fb6b2654364e65bbbfadf69b272632f5
niville <bayehniville@gmail.com>
2022-01-03 09:43:59 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
-  const[count] = useState(0);
+  const[count, setCount] = useState(0);
   return (
     <div className="container">
       <div className='card text-center my-5'>
@@ -10,9 +10,9 @@ function App() {
           <h1>Counter App</h1>
           <div className='my-5'>
             <h1 className='my-5'>{count}</h1>
-            <button className='btn btn-success mx-3'>Increment</button>
-            <button className='btn btn-danger mx-3'>Decrement</button>
-            <button className='btn btn-secondary mx-3'>Reset</button>
+            <button className='btn btn-success mx-3' onClick={() => setCount (count + 1)}>Increment</button>
+            <button className='btn btn-danger mx-3'onClick={() => setCount (count - 1)}>Decrement</button>
+            <button className='btn btn-secondary mx-3'onClick={() => setCount(0)}>Reset</button>
           </div>
         </div>
       </div>
